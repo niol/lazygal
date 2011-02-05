aa3910d9620a3914c98420e93ebacabda8b76556
Alexandre Rossi <alexandre.rossi@gmail.com>
2011-02-05 01:49:48 +0100

    use new pyexiv2 api wrapper in set comment script

--- lazygal-setcomment.py
+++ lazygal-setcomment.py
@@ -1,14 +1,14 @@
 import sys
 
-import pyexiv2
+from lazygal import pyexiv2api as pyexiv2
 
 fn = sys.argv[1]
 comment = sys.argv[2]
 
-im = pyexiv2.Image(fn)
-im.readMetadata()
+im = pyexiv2.ImageMetadata(fn)
+im.read()
 
 # Assume comment is in utf-8, more encoding processing example using
 # sys.stdin.encoding and example processing in lazygal/metadata.py
 im['Exif.Photo.UserComment'] = comment
-im.writeMetadata()
+im.write()
