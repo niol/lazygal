516b56382646eecb7f96ea058badedbd3ac7a5fc
Alexandre Rossi <alexandre.rossi@gmail.com>
2011-02-05 01:34:23 +0100

    unit tests for copied metadata in resized image

--- lazygaltest/test_generators.py
+++ lazygaltest/test_generators.py
@@ -18,9 +18,12 @@
 
 import unittest
 import os
+import datetime
+
 from __init__ import LazygalTest
 from lazygal.generators import Album
 from lazygal.sourcetree import Directory
+from lazygal import pyexiv2api as pyexiv2
 
 
 class TestSourceTree(LazygalTest):
@@ -45,6 +48,45 @@ class TestSourceTree(LazygalTest):
         self.assertEqual(os.path.islink(symlink), True)
         # Test if that symlink point to the image in the source_dir
         self.assertEqual(os.path.realpath(symlink), img_path)
+
+    def test_metadata_osize_copy(self):
+        img_path = self.add_img(self.source_dir, 'md_filled.jpg')
+
+        # Add some metadata
+        gps_data = pyexiv2.ImageMetadata(self.get_sample_path('sample-with-gps.jpg'))
+        gps_data.read()
+        source_image = pyexiv2.ImageMetadata(img_path)
+        source_image.read()
+        gps_data.copy(source_image)
+        dummy_comment = 'nice photo'
+        source_image['Exif.Photo.UserComment'] = dummy_comment
+        dummy_date = datetime.datetime(2011, 2, 3, 12, 51, 43)
+        source_image['Exif.Photo.DateTimeDigitized'] = dummy_date
+        assert 'Exif.GPSInfo.GPSLongitude' in source_image.exif_keys
+        assert 'Exif.GPSInfo.GPSLatitude' in source_image.exif_keys
+        source_image.write()
+
+        # Generate album
+        dest_dir = self.get_working_path()
+        self.album.generate(dest_dir)
+
+        dest_img_path = os.path.join(dest_dir, 'md_filled_small.jpg')
+        dest_image = pyexiv2.ImageMetadata(dest_img_path)
+        dest_image.read()
+
+        # Check that metadata is still here for reduced pictures.
+        self.assertEqual(dest_image['Exif.Photo.UserComment'].value,
+                         dummy_comment)
+        self.assertEqual(dest_image['Exif.Photo.DateTimeDigitized'].value,
+                         dummy_date)
+
+        # Check that blacklised tags are not present anymore in the reduced
+        # picture.
+        def lat(): return dest_image['Exif.GPSInfo.GPSLongitude'].value
+        self.assertRaises(KeyError, lat)
+
+        def long(): return dest_image['Exif.GPSInfo.GPSLatitude'].value
+        self.assertRaises(KeyError, long)
 
 
 if __name__ == '__main__':
