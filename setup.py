b44c2ba76b69376a84a455464117fcdf48bb8c2e
Alexandre Rossi <alexandre.rossi@gmail.com>
2011-04-04 09:31:51 +0200

    fix build with python2.7

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from distutils.core import setup, Command
+import distutils.sysconfig
 import distutils.command.build_scripts
 import distutils.command.build
 from distutils.dep_util import newer
