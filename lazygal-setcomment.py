956d57cbb2986981f7f41835c132b6d2f78ea0f7
Alexandre Rossi <alexandre.rossi@gmail.com>
2011-07-20 23:00:03 +0200

    make lazygal-setcomment compatible with non-ascii filesystems

--- lazygal-setcomment.py
+++ lazygal-setcomment.py
@@ -7,7 +7,7 @@ from lazygal import pyexiv2api as pyexiv2
 fn = sys.argv[1]
 comment = sys.argv[2]
 
-im = pyexiv2.ImageMetadata(fn)
+im = pyexiv2.ImageMetadata(fn.decode(sys.getfilesystemencoding()))
 im.read()
 
 # Assume comment is in utf-8, more encoding processing example using
