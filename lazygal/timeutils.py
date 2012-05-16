f382f1341ae8e18747b2e2572ed90dfd2014d60d
Alexandre Rossi <alexandre.rossi@gmail.com>
2012-05-16 11:36:17 +0200

    add header/footer in new module

--- lazygal/timeutils.py
+++ lazygal/timeutils.py
@@ -1,3 +1,21 @@
+# Lazygal, a lazy satic web gallery generator.
+# Copyright (C) 2012 Alexandre Rossi <alexandre.rossi@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 import datetime, locale
 
 
@@ -11,3 +29,6 @@ class unicode_datetime(datetime.datetime):
 def unicodify_datetime(dt):
     return unicode_datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute,
                             dt.second, dt.microsecond, dt.tzinfo)
+
+
+# vim: ts=4 sw=4 expandtab
